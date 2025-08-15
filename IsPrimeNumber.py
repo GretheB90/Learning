ad94d68a2e12975e591e5f63169610211a1961d7
Grethe_B <47736754+GretheB90@users.noreply.github.com>
2025-08-16 00:02:43 +0200

    Create IsPrimeNumber.py
    Adding a Prime number checked script

---
+++ IsPrimeNumber.py
@@ -0,0 +1,15 @@
+nums = range(2,1000) #List of numbers from 2 and all the way to 999.
+
+def is_prime(num):
+    for x in range(2,num):
+        if (num%x) == 0:
+            return False
+    return True
+
+Primes = list(filter(is_prime, nums))
+
+print(Primes)
+
+#If any of those numbers divides evenly (no remainder), it's not a prime
+#If nothing divides it evenly, it's a prime!
+#Prime number: Only divisible with 1 and itself.
